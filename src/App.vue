c08e42a812452b8b4d080783e49a82bb426fc2c5
Jinlin <18081569d@gmail.com>
2020-09-07 14:33:14 +0800

    First Commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app" style="background-color: green;width: 100%;height: 100%;position: absolute">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Home/>
+  </div>
+</template>
+
+<script>
+import Home from './views/Home.vue'
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Home, HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #FFFF00;
+  margin-top: 60px;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="home" style="background-color: red;width: 500px;height: 100px;">
+    <img alt="Vue logo" src="../assets/logo.png">
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home'
+}
+</script>
